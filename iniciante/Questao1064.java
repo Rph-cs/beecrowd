da217db67574a42e3167ec58a60c1e2b0e013a39
Rph.cs <raphael.dev.silva@gmail.com>
2026-04-04 22:18:45 -0300

    Treinando Java no Beecrowd

--- iniciante/Questao1064.java
+++ iniciante/Questao1064.java
@@ -9,10 +9,20 @@ public class Questao1064 {
         Scanner scanner = new Scanner(System.in);
 
         double[] numeros = new double[6];
-
+        int quantidadePositivos = 0;
+        double somaPositivos = 0;
+        
         for (int i = 0; i < 6; i++) {
-
+            numeros[i] = scanner.nextDouble();
+            
+            if (numeros[i] > 0) {
+                quantidadePositivos++;
+                somaPositivos += numeros[i];
+            }
         }
+        System.out.println(quantidadePositivos + " valores positivos");
+        somaPositivos = somaPositivos / quantidadePositivos;
+        System.out.printf("%.1f\n", somaPositivos);
 
     }
 }
